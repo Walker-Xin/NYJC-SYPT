4ed1366ae4db0e097f2363c86657d1f9a9c2304d
Xin Wenkang <walkerxin@outlook.com>
2020-11-26 17:02:47 +0000

    Modified bead dynamics script

--- bead_dynamics.py
+++ bead_dynamics.py
@@ -14,7 +14,7 @@ I = 0.5 * m_0 * (r**2)
 M = (m * (np.sin(phi_0)**2) * (r**2) + I) * theta_dot_0
 E = 0.5 * m * (np.sin(phi_0)**2) * (r**2) * (theta_dot_0**2) + 0.5 * I * (theta_dot_0**2) + m * g * r * (1 + np.cos(phi_0))
 
-# Defining integrals
+# Defining integral and gradient functions
 def t_integral(phi):
     c_1 = 2 * E / (m*(r**2))
     c_2 = M**2
@@ -23,6 +23,7 @@ def t_integral(phi):
     c_5 = 2 * g / r
     integral = 1 / np.sqrt(c_1 - c_2 / (c_3 * (np.sin(phi)**2) + c_4) - c_5 * (1 + np.cos(phi)))
     return integral
+
 
 def theta_integral(phi):
     c_1 = 2 * E / (m*r*r)
@@ -34,6 +35,7 @@ def theta_integral(phi):
     integral_2 = 1 / np.sqrt(c_1 - c_2 / (c_3 * (np.sin(phi)**2) + c_4) - c_5 * (1 + np.cos(phi)))
     integral = integral_1 * integral_2
     return integral
+
 
 # Setting upper bound
 phi_f = 360/180 * np.pi
@@ -47,19 +49,35 @@ for i in range(phi_range.size):
 for i in range(phi_range.size): 
     theta_range[i], error = integrate.quad(theta_integral, phi_0, phi_range[i])
 
-print(phi_range, theta_range, t_range)
+'''print(phi_range)
+print(theta_range)
+print(t_range)'''
+
+# Computing gradients
+theta_grad = np.diff(theta_range) / np.diff(t_range)
+phi_grad = np.diff(phi_range) / np.diff(t_range)
+theta_grad = np.append(theta_grad, theta_grad[-1])
+phi_grad = np.append(phi_grad, phi_grad[-1])
 
 # Visualisation
-fig, axs = plt.subplots(1, 2, figsize=(12, 7))
+fig, axs = plt.subplots(2, 2, figsize=(12, 7))
 
-axs[0].plot(t_range, theta_range)
-axs[0].set_yticks(np.arange(np.pi, 3*np.pi, np.pi))
-axs[0].set_xlabel('Time')
-axs[0].set_ylabel('Theta')
-axs[1].plot(t_range, phi_range)
-axs[1].set_yticks(np.arange(np.pi, 3*np.pi, np.pi))
-axs[1].set_xlabel('Time')
-axs[1].set_ylabel('Phi')
+axs[0][0].plot(t_range, theta_range)
+axs[0][0].set_yticks(np.arange(np.pi, 3*np.pi, np.pi))
+axs[0][0].set_xlabel('Time')
+axs[0][0].set_ylabel('Theta')
+axs[0][1].plot(t_range, phi_range)
+axs[0][1].set_yticks(np.arange(np.pi, 3*np.pi, np.pi))
+axs[0][1].set_xlabel('Time')
+axs[0][1].set_ylabel('Phi')
+axs[1][0].plot(t_range, theta_grad)
+# axs[1][0].set_yticks(np.arange(np.pi, 3*np.pi, np.pi))
+axs[1][0].set_xlabel('Time')
+axs[1][0].set_ylabel('Theta_dot')
+axs[1][1].plot(t_range, phi_grad)
+# axs[1][1].set_yticks(np.arange(np.pi, 3*np.pi, np.pi))
+axs[1][1].set_xlabel('Time')
+axs[1][1].set_ylabel('Phi_dot')
 
 plt.show()
 plt.close()
