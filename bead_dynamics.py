c22bcada4ea8d465afcac93f1ed4b237e5b8c346
Xin Wenkang <walkerxin@outlook.com>
2020-11-30 20:28:00 +0800

    Added comments; Modified formatting

--- bead_dynamics.py
+++ bead_dynamics.py
@@ -3,39 +3,43 @@ import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
 import warnings
-warnings.simplefilter('error')
+# warnings.simplefilter('error') # Uncomment to make script stop should a warning is raised
 
 # Setting parameters
-m = 0.2
-m_0 = 0.5
-r = 0.1
-phi_0 = 30/180 * np.pi
-theta_dot_0 = np.pi/1
-g = 9.81
+m = 0.2  # Mass of the bead in kilogram
+m_0 = 0.5  # Mass of the loop in kilogram
+r = 0.1  # Radius of the loop in meter
+phi_0 = 30/180 * np.pi # Initial angular displacement from +y-axis clockwise of the bead in radian
+omega = np.pi/1  # Initial angular velocity of the loop in radian per second
+g = 9.81  # Gravitational accelertaion in meter per second^2
 
-I = 0.5 * m_0 * (r**2)
-M = (m * (np.sin(phi_0)**2) * (r**2) + I) * theta_dot_0
-E = 0.5 * m * (np.sin(phi_0)**2) * (r**2) * (theta_dot_0**2) + 0.5 * I * (theta_dot_0**2) + m * g * r * (1 + np.cos(phi_0))
+I = 0.5 * m_0 * (r**2)  # Moment of inertia of the loop about the diameter
+M = (m * (np.sin(phi_0)**2) * (r**2) + I) * \omega  # Initial angular momentum of the system
+E = 0.5 * m * (np.sin(phi_0)**2) * (r**2) * (omega**2) + 0.5 * I * (omega **2) + m * g * r * (1 + np.cos(phi_0))  # Initial energy of the system
 
-# Defining integral and gradient functions
+# Defining integral functions
+
+
 def t_integral(phi):
+    # Expressing time as a function of phi
     c_1 = 2 * E / (m*(r**2))
     c_2 = M**2
     c_3 = (m**2) * (r**4)
     c_4 = I * m * (r**2)
     c_5 = 2 * g / r
-    integral = 1 / np.sqrt(c_1 - c_2 / (c_3 * (np.sin(phi)**2) + c_4) - c_5 * (1 + np.cos(phi)))
+    integral = 1 / \np.sqrt(c_1 - c_2 / (c_3 * (np.sin(phi)**2) + c_4) - c_5 * (1 + np.cos(phi)))
     return integral
 
 
 def theta_integral(phi):
+    # Expressing theta as a function of phi
     c_1 = 2 * E / (m*r*r)
     c_2 = M**2
     c_3 = (m**2) * (r**4)
     c_4 = I * m * (r**2)
     c_5 = 2 * g / r
     integral_1 = M / ((m * (r**2) * (np.sin(phi)**2)) + I)
-    integral_2 = 1 / np.sqrt(c_1 - c_2 / (c_3 * (np.sin(phi)**2) + c_4) - c_5 * (1 + np.cos(phi)))
+    integral_2 = 1 / \np.sqrt(c_1 - c_2 / (c_3 * (np.sin(phi)**2) + c_4) - c_5 * (1 + np.cos(phi)))
     integral = integral_1 * integral_2
     return integral
 
@@ -47,10 +51,10 @@ phi_f = 360/180 * np.pi
 phi_range = np.linspace(phi_0, phi_f, num=100)
 t_range = np.zeros(phi_range.size)
 theta_range = np.zeros(phi_range.size)
-for i in range(phi_range.size): 
+for i in range(phi_range.size):
     print('t', i, 'phi', phi_range[i])
     t_range[i], error = integrate.quad(t_integral, phi_0, phi_range[i])
-for i in range(phi_range.size): 
+for i in range(phi_range.size):
     print('theta', i, 'phi', phi_range[i])
     theta_range[i], error = integrate.quad(theta_integral, phi_0, phi_range[i])
 
--- bead_dynamics_2.py
+++ bead_dynamics_2.py
@@ -2,39 +2,45 @@ import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
-# Set constants
-omega = np.pi/0.5
-g = 9.81
-r = 0.1
+# Setting constants
+omega = np.pi/0.5  # Angular velocity of the loop in radian per second
+g = 9.81  # Gravitational accelertaion in meter per second^2
+r = 0.1  # Radius of the loop in meter
 
 A = omega**2
 B = g / r
 
-phi_0 = 0
-phi_dot_0 = 0.01
+phi_0 = 0  # Initial angular displacement from -y-axis anticlockwise of the bead in radian
+phi_dot_0 = 0.01  # Initial angular velocity from -y-axis anticlockwise of the bead in radian
 
 
+# Defining differential equations
 def dy_dt(y, t):
     # Solving the equation phi_dotdot = A*sin(phi)*cos(phi) - B*sin(phi)
     # Let y be a vector y = [phi_1, phi_2], where phi_1 = phi and phi_2 = phi_dot
     return [y[1], A*np.sin(y[0])*np.cos(y[0]) - B*np.sin(y[0])]
 
 
-y_0 = [phi_0, phi_dot_0] # Set initial values
+# Generating data
+y_0 = [phi_0, phi_dot_0]  # Setting initial values
 t_range = np.linspace(0, 10, 300)
 y_range = integrate.odeint(dy_dt, y_0, t_range)
-phi_range = y_range[:,0]
+phi_range = y_range[:, 0]
 
 '''print(phi_range)
 print(t_range)
 print(len(phi_range), len(t_range))'''
 
+# Visualisation
 plt.xlabel('t/s')
 plt.ylabel('$\phi$ /rad')
 
-plt.text(0.85, 0.15, r'$\dot \phi_0$ = {} rad'.format(phi_dot_0), {'fontsize': 10}, transform=plt.gca().transAxes)
-plt.text(0.85, 0.10, r'$\omega$ = {} rad/s'.format(round(omega, 2)), {'fontsize': 10}, transform=plt.gca().transAxes)
-plt.text(0.85, 0.05, r'$r$ = {} m'.format(round(r, 2)), {'fontsize': 10}, transform=plt.gca().transAxes)
+plt.text(0.85, 0.15, r'$\dot \phi_0$ = {} rad'.format(phi_dot_0),
+         {'fontsize': 10}, transform=plt.gca().transAxes)
+plt.text(0.85, 0.10, r'$\omega$ = {} rad/s'.format(round(omega, 2)),
+         {'fontsize': 10}, transform=plt.gca().transAxes)
+plt.text(0.85, 0.05, r'$r$ = {} m'.format(round(r, 2)), {
+         'fontsize': 10}, transform=plt.gca().transAxes)
 
 plt.plot(t_range, phi_range)
 plt.show()
